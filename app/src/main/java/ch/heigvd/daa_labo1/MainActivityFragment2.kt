d13cf4f6a554ea4b88440095ad9e34a35a3a687d
Maxime Scharwath <maxscharwath@gmail.com>
2022-10-14 10:21:05 +0200

    step 3 start

---
+++ app/src/main/java/ch/heigvd/daa_labo1/MainActivityFragment2.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.daa_labo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityFragment2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_fragment2)
+    }
+}
---
+++ app/src/main/java/ch/heigvd/daa_labo1/fragment/StepperFragment.kt
@@ -0,0 +1,61 @@
+package ch.heigvd.daa_labo1.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import ch.heigvd.daa_labo1.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_STEP = "ARG_STEP"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StepperFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StepperFragment : Fragment() {
+    private var stepValue: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            stepValue = it.getString(ARG_STEP)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stepper, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textStep = view.findViewById<TextView>(R.id.text_step)
+        textStep.text = stepValue
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @return A new instance of fragment StepperFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String) =
+            StepperFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_STEP, param1)
+                }
+            }
+    }
+}
